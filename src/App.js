862b1aae8407fa9b3cf0edb1780d0581d6f82d51
unknown <malvikasinha1908@gmail.com>
2024-09-14 14:32:55 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js  
+import React from 'react';  
+import FrontPage from './components/FrontPage';  
+import Modules from './components/Modules';  
+
+function App() {  
+  return (  
+    <div>  
+      <FrontPage />  
+      <Modules />
+    </div>  
+  );  
+}  
+
+export default App;
